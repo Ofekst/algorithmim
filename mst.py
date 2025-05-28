f9aefd2e36d0cc862950d6f28c96b435935e0b18
Ofekst <Ofekstrteicher0@gmail.com>
2025-05-28 16:57:34 +0300

    add return values

--- mst.py
+++ mst.py
@@ -1,6 +1,6 @@
 import random
 
-def create_graph(num_vertices:int, num_edges:int):
+def create_graph(num_vertices:int, num_edges:int) -> (dict[int,list[int]],list[int]):
     """
     Creates a connected undirected weighted graph
 
